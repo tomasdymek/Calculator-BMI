8e45a5544d5e37887d2f5eab4e75c043a1b11090
Tomasz Ciesla <tomasz.ciesla81@gmail.com>
2023-06-01 18:29:34 +0200

    Added blog app

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+
+from django.db import models
+
+class BMI(models.Model):
+    weight = models.FloatField()
+    height = models.FloatField()
+    bmi = models.FloatField()
+
+    def __str__(self):
+        return f"BMI: {self.bmi}"
---
+++ blog/views.py
@@ -0,0 +1,23 @@
+
+from django.shortcuts import render
+from .models import BMI
+
+def starting_page(request):
+    return render(request, "blog/index.html")
+
+def posts(request):
+    return render(request,"blog/all-posts.html")
+
+def post_detail(request):
+    pass
+
+
+def calculate_bmi(request):
+    if request.method == 'POST':
+        weight = float(request.POST['weight'])
+        height = float(request.POST['height'])
+        bmi_value = weight / ((height/100) ** 2)  # Obliczanie BMI
+        bmi_obj = BMI.objects.create(weight=weight, height=height, bmi=bmi_value)
+        bmi_obj.save()
+        return render(request, 'result.html', {'bmi': bmi_obj})
+    return render(request, 'index.html')
