4aabc1efa5929c02781d5fc1d651264273f5f5ae
tomasdymek <tomasdymek@gmail.com>
2023-06-07 17:59:47 +0200

    Added css

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
-
 from django.shortcuts import render
 from .models import BMI
+
 
 def starting_page(request):
     return render(request, "blog/index.html")
 
+
 def posts(request):
-    return render(request,"blog/all-posts.html")
+    return render(request, "blog/all-posts.html")
+
 
 def post_detail(request):
     pass
@@ -16,8 +18,25 @@ def calculate_bmi(request):
     if request.method == 'POST':
         weight = float(request.POST['weight'])
         height = float(request.POST['height'])
-        bmi_value = weight / ((height/100) ** 2)  # Obliczanie BMI
+        bmi_value = int(weight / ((height / 100) ** 2))  # Obliczanie BMI
         bmi_obj = BMI.objects.create(weight=weight, height=height, bmi=bmi_value)
         bmi_obj.save()
-        return render(request, 'result.html', {'bmi': bmi_obj})
-    return render(request, 'index.html')
+
+        # Opisy wyników BMI
+        if bmi_value < 18.5:
+            description = "Underweight - You should eat more."
+            bmi_category = 'underweight'
+        elif bmi_value >= 18.5 and bmi_value < 25:
+            description = "Normal weight - Keep up the good work!"
+            bmi_category = 'normal'
+        elif bmi_value >= 25 and bmi_value < 30:
+            description = "Overweight - You should eat less."
+            bmi_category = 'overweight'
+        else:
+            description = "Obese - You should consult a doctor."
+            bmi_category = 'obese'
+
+        return render(request, 'blog/result.html', {'bmi': bmi_obj, 'description': description, 'bmi_category': bmi_category})
+
+    return render(request, 'blog/index.html')
+
